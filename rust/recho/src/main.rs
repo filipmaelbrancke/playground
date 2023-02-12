0b13fc9fb407f1700040c2557f0e269c08ce621a
Filip Maelbrancke <filip@maelbrancke.net>
2023-02-12 20:31:13 +0100

    cargo fmt

--- rust/recho/src/main.rs
+++ rust/recho/src/main.rs
@@ -1,7 +1,6 @@
 use clap::{App, Arg};
 
 fn main() {
-
     let matches = App::new("recho")
         .version("0.1.0")
         .author("Filip Maelbrancke")
@@ -11,18 +10,18 @@ fn main() {
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
-                .min_values(1)
+                .min_values(1),
         )
         .arg(
             Arg::with_name("omit_newline")
                 .short("n")
                 .help("Do not print newline")
-                .takes_value(false)
+                .takes_value(false),
         )
         .get_matches();
 
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
 
-    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" } );
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
