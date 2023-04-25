08f1abd3e0e0e01bc635f14601117f833c96f337
Filip Maelbrancke <filip@maelbrancke.net>
2023-04-25 23:07:11 +0200

    api: http client

--- rust/api/src/main.rs
+++ rust/api/src/main.rs
@@ -2,6 +2,7 @@ use std::io::Error;
 use std::net::TcpListener;
 
 use api::configuration::get_configuration;
+use api::email_client::EmailClient;
 use api::startup::run;
 use api::telemetry;
 use sqlx::postgres::PgPoolOptions;
@@ -22,10 +23,20 @@ async fn main() -> Result<(), Error> {
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
 
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
--- rust/api/tests/health_check.rs
+++ rust/api/tests/health_check.rs
@@ -1,4 +1,5 @@
 use api::configuration::{get_configuration, DatabaseSettings};
+use api::email_client::EmailClient;
 use api::startup::run;
 use api::telemetry::{get_subscriber, init_subscriber};
 use once_cell::sync::Lazy;
@@ -145,7 +146,17 @@ async fn spawn_api_app() -> TestApp {
 
     let connection_pool = configure_database(&configuration.database).await;
 
-    let server = run(listener, connection_pool.clone()).expect("Failed to bind");
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
+
+    let server = run(listener, connection_pool.clone(), email_client).expect("Failed to bind");
     // launch server as background task
     // tokio::spawn returns handle to spawned future
     // not used here -> hence non-binding let
