cef67bfe5c474b68614418db567ed0086a007d2a
Filip Maelbrancke <filip@maelbrancke.net>
2023-05-15 23:20:21 +0200

    api: idiomatic error handling

--- rust/api/src/routes/subscriptions.rs
+++ rust/api/src/routes/subscriptions.rs
@@ -3,6 +3,7 @@ use std::fmt::Formatter;
 
 use actix_web::http::StatusCode;
 use actix_web::{web, HttpResponse, ResponseError};
+use anyhow::Context;
 use chrono::Utc;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
@@ -40,9 +41,19 @@ pub enum SubscribeError {
     #[error("{0}")]
     ValidationError(String),
     // transparent : delegate Display and source implementations to the type wrapped by `UnexpectedError`
-    // #[error(transparent)]
-    #[error("{1}")]
-    UnexpectedError(#[source] Box<dyn std::error::Error>, String),
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+    //UnexpectedError(#[source] Box<dyn std::error::Error>, String),
+    // wrap `std::error::Error` into a `Box` since size of trait objects is unknown at compile-time
+    // (trait objects can store different types which can have different layout in memory - and are
+    // this unsized / don't implement the Sized marker trait. A `Box` stores the trait object on
+    // the heap, while the pointer to its heap location is stored in `UnexpectedError`
+    // and the pointer itself has a known size at compile-time -> Rust happy, `Sized` again)
+    // `anyhow::Error` is a wrapper around a dynamic error type. Somewhat similar to `Box<dyn std::error::Error>
+    // but different:
+    // - anyhow::Error requires that the error is Send, Sync and 'static
+    // - anyhow::Error guarantees a backtrace is available
+    // - anyhow::Error is represented as a narrow pointer (exactly one word in size instead of two)
 }
 
 impl std::fmt::Debug for SubscribeError {
@@ -55,7 +66,7 @@ impl ResponseError for SubscribeError {
     fn status_code(&self) -> StatusCode {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::UnexpectedError(_, _) => StatusCode::INTERNAL_SERVER_ERROR,
+            SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
@@ -80,38 +91,27 @@ pub async fn subscribe(
     // `form.0` -> access to underlying `FormData`
     let new_subscriber = form.0.try_into().map_err(SubscribeError::ValidationError)?;
 
-    let mut transaction = pool.begin().await.map_err(|e| {
-        SubscribeError::UnexpectedError(
-            Box::new(e),
-            "Failed to acquire Postgres connection from the pool".into(),
-        )
-    })?;
+    let mut transaction = pool
+        .begin()
+        .await
+        // context converts error returned into an anyhow::Error and enriches it with
+        // additional context around the intentions of the caller
+        // anyhow implements the Context trait for Result
+        .context("Failed to acquire Postgres connection from the pool")?;
 
     let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(
-                Box::new(e),
-                "Failed to insert new subscriber in database".into(),
-            )
-        })?;
+        .context("Failed to insert new subscriber in database")?;
 
     let subscription_token = generate_subscription_token();
     store_token(&mut transaction, subscriber_id, &subscription_token)
         .await
-        .map_err(|e| {
-            SubscribeError::UnexpectedError(
-                Box::new(e),
-                "Failed to store confirmation token for new subscriber".into(),
-            )
-        })?;
+        .context("Failed to store confirmation token for new subscriber")?;
 
-    transaction.commit().await.map_err(|e| {
-        SubscribeError::UnexpectedError(
-            Box::new(e),
-            "Failed to commit SQL transaction to store new subscriber".into(),
-        )
-    })?;
+    transaction
+        .commit()
+        .await
+        .context("Failed to commit SQL transaction to store new subscriber")?;
 
     send_confirmation_email(
         &email_client,
@@ -120,9 +120,7 @@ pub async fn subscribe(
         &subscription_token,
     )
     .await
-    .map_err(|e| {
-        SubscribeError::UnexpectedError(Box::new(e), "Failed to send confirmation email".into())
-    })?;
+    .context("Failed to send confirmation email")?;
     Ok(HttpResponse::Ok().finish())
 }
 
