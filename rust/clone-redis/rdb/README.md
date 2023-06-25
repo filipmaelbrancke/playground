63573970a08b4b7140560456a65db3a21b89472f
Filip Maelbrancke <filip@maelbrancke.net>
2023-06-25 16:02:42 +0200

    redis-clone: client: GET/SET

--- rust/clone-redis/rdb/README.md
+++ rust/clone-redis/rdb/README.md
@@ -3,4 +3,7 @@ Rust Redis client
 
 ```
 cargo run -q -- ping
+
+cargo run -- SET greeting hello
+cargo run -- GET greeting
 ```
--- rust/clone-redis/rdb/src/main.rs
+++ rust/clone-redis/rdb/src/main.rs
@@ -4,6 +4,8 @@ use std::net::TcpStream;
 #[derive(Debug)]
 enum Command {
     Ping,
+    Get { key: String },
+    Set { key: String, value: String },
 }
 
 #[derive(Debug)]
@@ -14,12 +16,49 @@ struct Config {
 
 impl Command {
     fn send<W: Write>(&self, dest: &mut W) -> std::io::Result<usize> {
-        let written = match self {
+        let bytes_written = match self {
             Command::Ping => dest.write(b"*1\r\n$4\r\nPING\r\n")?,
+            Command::Get { key } => {
+                let args = &[key.as_bytes()];
+                encode_and_write(dest, b"GET", args)?
+            }
+            Command::Set { key, value } => {
+                let args = &[key.as_bytes(), value.as_bytes()];
+                encode_and_write(dest, b"SET", args)?
+            }
         };
 
-        Ok(written)
+        Ok(bytes_written)
     }
+}
+
+fn encode_and_write<W: Write>(
+    dest: &mut W,
+    command: &[u8],
+    args: &[&[u8]],
+) -> std::io::Result<usize> {
+    let n_args = format!("{}", 1 + args.len());
+    let mut bytes_written = 0;
+
+    bytes_written += dest.write(b"*")?;
+    bytes_written += dest.write(n_args.as_bytes())?;
+    bytes_written += dest.write(b"\r\n")?;
+
+    bytes_written += dest.write(b"$")?;
+    bytes_written += dest.write(command.len().to_string().as_bytes())?;
+    bytes_written += dest.write(b"\r\n")?;
+    bytes_written += dest.write(command)?;
+    bytes_written += dest.write(b"\r\n")?;
+
+    for arg in args {
+        bytes_written += dest.write(b"$")?;
+        bytes_written += dest.write(arg.len().to_string().as_bytes())?;
+        bytes_written += dest.write(b"\r\n")?;
+        bytes_written += dest.write(arg)?;
+        bytes_written += dest.write(b"\r\n")?;
+    }
+
+    Ok(bytes_written)
 }
 
 impl Config {
@@ -30,6 +69,13 @@ impl Config {
         let pos = 1;
         let command = match args[pos].to_uppercase().as_str() {
             "PING" => Command::Ping,
+            "GET" => Command::Get {
+                key: args[pos + 1].clone(),
+            },
+            "SET" => Command::Set {
+                key: args[pos + 1].clone(),
+                value: args[pos + 2].clone(),
+            },
             _ => todo!("{}", args[pos]),
         };
 
@@ -42,8 +88,8 @@ fn main() -> std::io::Result<()> {
     // dbg!(&config);
 
     let mut stream = TcpStream::connect(config.host)?;
-    let command = config.command;
-    command.send(&mut stream)?;
+    let bytes_sent = config.command.send(&mut stream)?;
+    eprintln!("info: {bytes_sent} bytes sent");
 
     let mut buffer = [0; 1024];
     let _ = stream.read(&mut buffer[..])?;
