ce407259666c3908f2f2c3a79659f0a16c68efd0
Filip Maelbrancke <filip@maelbrancke.net>
2023-12-17 22:41:51 +0100

    advent of code day 4 part 2

--- rust/puzzles-aoc-2023/src/day04.rs
+++ rust/puzzles-aoc-2023/src/day04.rs
@@ -7,6 +7,9 @@ pub fn solve() {
 
     let result_part1 = calculate_total_points(input.clone());
     println!("Result part 1: {result_part1}");
+
+    let result_part2 = calculate_number_of_scratchcards(input.clone());
+    println!("Result part 2: {result_part2}");
 }
 
 pub fn calculate_total_points(input: String) -> u32 {
@@ -20,10 +23,35 @@ pub fn calculate_total_points(input: String) -> u32 {
     result
 }
 
+pub fn calculate_number_of_scratchcards(input: String) -> u32 {
+    // usize is preferred above u32, because usize is the default type for enumerating
+    let matches_per_game: Vec<usize> = input
+        .lines()
+        .map(parse_game_line)
+        .map(check_amount_of_successful_numbers_as_usize)
+        .collect();
+
+    let mut copies_per_game: Vec<u32> = vec![1; matches_per_game.len()];
+
+    for (i, amount_of_matches) in matches_per_game.into_iter().enumerate() {
+        for j in 0..amount_of_matches {
+            copies_per_game[i + j + 1] += copies_per_game[i];
+        }
+    }
+
+    copies_per_game.iter().sum()
+}
+
 fn check_amount_of_successful_numbers(
     (winning_numbers, game_numbers): (HashSet<u32>, HashSet<u32>),
 ) -> u32 {
     game_numbers.intersection(&winning_numbers).count() as u32
+}
+
+fn check_amount_of_successful_numbers_as_usize(
+    (winning_numbers, game_numbers): (HashSet<u32>, HashSet<u32>),
+) -> usize {
+    check_amount_of_successful_numbers((winning_numbers, game_numbers)) as usize
 }
 
 fn parse_game_line(game_line: &str) -> (HashSet<u32>, HashSet<u32>) {
@@ -48,7 +76,8 @@ fn parse_game_line(game_line: &str) -> (HashSet<u32>, HashSet<u32>) {
 #[cfg(test)]
 mod tests {
     use crate::day04::{
-        calculate_total_points, check_amount_of_successful_numbers, parse_game_line,
+        calculate_number_of_scratchcards, calculate_total_points,
+        check_amount_of_successful_numbers, parse_game_line,
     };
     use crate::get_input_as_string;
     use std::collections::HashSet;
@@ -76,5 +105,11 @@ mod tests {
     fn test_aoc_example() {
         let test_input = get_input_as_string("day04", "example");
         assert_eq!(calculate_total_points(test_input), 13);
+    }
+
+    #[test]
+    fn test_aoc_example2() {
+        let test_input = get_input_as_string("day04", "example");
+        assert_eq!(calculate_number_of_scratchcards(test_input), 30);
     }
 }
